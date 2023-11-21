85e7a98173e19afe8a19e931e05271c62b68eae7
Cappen <Cappen@DESKTOP-NTS5CSB>
2023-11-21 15:55:13 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Calculator
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.calculate = new System.Windows.Forms.Button();
+            this.operand1 = new System.Windows.Forms.TextBox();
+            this.operand2 = new System.Windows.Forms.TextBox();
+            this.operationsList = new System.Windows.Forms.ComboBox();
+            this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // calculate
+            // 
+            this.calculate.Location = new System.Drawing.Point(131, 168);
+            this.calculate.Name = "calculate";
+            this.calculate.Size = new System.Drawing.Size(133, 43);
+            this.calculate.TabIndex = 0;
+            this.calculate.Text = "calculate";
+            this.calculate.UseVisualStyleBackColor = true;
+            this.calculate.Click += new System.EventHandler(this.calculate_Click);
+            // 
+            // operand1
+            // 
+            this.operand1.Location = new System.Drawing.Point(12, 88);
+            this.operand1.Name = "operand1";
+            this.operand1.Size = new System.Drawing.Size(100, 20);
+            this.operand1.TabIndex = 1;
+            this.operand1.TextChanged += new System.EventHandler(this.operand1_TextChanged);
+            this.operand1.Enter += new System.EventHandler(this.operand1_Enter);
+            this.operand1.Validating += new System.ComponentModel.CancelEventHandler(this.operand1_Validating);
+            // 
+            // operand2
+            // 
+            this.operand2.Location = new System.Drawing.Point(269, 87);
+            this.operand2.Name = "operand2";
+            this.operand2.Size = new System.Drawing.Size(100, 20);
+            this.operand2.TabIndex = 2;
+            this.operand2.TextChanged += new System.EventHandler(this.operand2_TextChanged);
+            this.operand2.Enter += new System.EventHandler(this.operand2_Enter);
+            this.operand2.Validating += new System.ComponentModel.CancelEventHandler(this.operand2_Validating);
+            // 
+            // operationsList
+            // 
+            this.operationsList.FormattingEnabled = true;
+            this.operationsList.Location = new System.Drawing.Point(131, 87);
+            this.operationsList.Name = "operationsList";
+            this.operationsList.Size = new System.Drawing.Size(121, 21);
+            this.operationsList.TabIndex = 3;
+            this.operationsList.SelectedIndexChanged += new System.EventHandler(this.operationsList_SelectedIndexChanged);
+            // 
+            // errorProvider
+            // 
+            this.errorProvider.ContainerControl = this;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Operand 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(266, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Operand 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(128, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Operation";
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(163, 137);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(58, 13);
+            this.resultLabel.TabIndex = 7;
+            this.resultLabel.Text = "resultLabel";
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 255);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.operationsList);
+            this.Controls.Add(this.operand2);
+            this.Controls.Add(this.operand1);
+            this.Controls.Add(this.calculate);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Calculator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculator";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button calculate;
+        private System.Windows.Forms.TextBox operand1;
+        private System.Windows.Forms.TextBox operand2;
+        private System.Windows.Forms.ComboBox operationsList;
+        private System.Windows.Forms.ErrorProvider errorProvider;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Calculator : Form
+    {
+
+        private const string NumberErrorMessage = "Should be a number";
+
+        public enum Operation
+        {
+            Addition = 0,
+            Subtraction = 1,
+            Multiplication = 2,
+            Division = 3
+        }
+        public Calculator()
+        {
+            InitializeComponent();
+
+            operationsList.DataSource = Enum.GetValues(typeof(Operation));
+            EnableDisableCalculateButton();
+        }
+
+        private void operand1_TextChanged(object sender, EventArgs e)
+        {
+
+            errorProvider.SetError(operand1, null);
+            EnableDisableCalculateButton();
+
+        }
+
+        private void operand2_TextChanged(object sender, EventArgs e)
+        {
+
+            errorProvider.SetError(operand2, null);
+            EnableDisableCalculateButton();
+        }
+
+        private void operand1_Validating(object sender, CancelEventArgs e)
+        {
+            TextBox textbox = (TextBox)sender;
+
+            if (!double.TryParse(textbox.Text, out _))
+            {
+                errorProvider.SetError(operand1, NumberErrorMessage);
+            }
+            else
+            {
+                errorProvider.SetError(operand1, null);
+            }
+
+            EnableDisableCalculateButton();
+        }
+
+        private void operand2_Validating(object sender, CancelEventArgs e)
+        {
+            TextBox textbox = (TextBox)sender;
+
+            if (!double.TryParse(textbox.Text, out _))
+            {
+                errorProvider.SetError(operand2, NumberErrorMessage);
+            }
+            else
+            {
+                errorProvider.SetError(operand2, null);
+            }
+
+            EnableDisableCalculateButton();
+        }
+
+        private void operand2_Enter(object sender, EventArgs e)
+        {
+            errorProvider.SetError(operand2, null);
+        }
+
+        private void operand1_Enter(object sender, EventArgs e)
+        {
+            errorProvider.SetError(operand1, null);
+        }
+
+        private void operationsList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            EnableDisableCalculateButton();
+        }
+
+        private void EnableDisableCalculateButton()
+        {
+            string op1 = operand1.Text.Replace(",", ".");
+            string op2 = operand2.Text.Replace(",", ".");
+
+            bool isValidOperand1 = double.TryParse(op1, out _);
+            bool isValidOperand2 = double.TryParse(op2, out _);
+            bool isOperationSelected = operationsList.SelectedIndex != -1;
+
+            calculate.Enabled = isValidOperand1 && isValidOperand2 && isOperationSelected;
+            resultLabel.Text = string.Empty;
+
+        }
+
+        private void calculate_Click(object sender, EventArgs e)
+        {
+            if (operationsList.SelectedItem != null)
+            {
+                Operation selectedOperation = (Operation)operationsList.SelectedItem;
+
+                string op1 = operand1.Text.Replace(",", ".");
+                string op2 = operand2.Text.Replace(",", ".");
+
+                double num1 = double.Parse(op1);
+                double num2 = double.Parse(op2);
+
+                double result = 0;
+
+                switch (selectedOperation)
+                {
+                    case Operation.Addition:
+                        result = num1 + num2;
+                        break;
+
+                    case Operation.Subtraction:
+                        result = num1 - num2;
+                        break;
+
+                    case Operation.Multiplication:
+                        result = num1 * num2;
+                        break;
+
+                    case Operation.Division:
+                        if (num2 != 0)
+                        {
+                            result = num1 / num2;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Cannot divide by zero.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                        }
+                        break;
+
+                    default:
+                        MessageBox.Show("Unknown operation selected.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                }
+
+                result = Math.Round(result, 2);
+
+                resultLabel.Text = $"Result: {result}";
+            }
+        }
+    }
+}
